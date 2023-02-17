d833e704441ada8c74ad2005ee4557d9ad41154f
YomnaMaged <yomnaMaged>
2023-02-16 18:54:30 -0800

    my first program with C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* program to print a statment 
+*without using printf
+*use puts 
+*/
+
+int main (void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
