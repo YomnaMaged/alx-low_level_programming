e0bc1956207c87c9ce421c1eae001b87ceb57f75
YomnaMaged <yomnaMaged>
2023-02-16 20:26:43 -0800

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<unlstd.h>
+/**
+ * main - program to print a statment without printf or puts
+ * Return: 0
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n, stdout);
+return (1);
+}
