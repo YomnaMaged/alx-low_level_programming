01782f4a0836001b066501c5cc305bd5bac2f129
YomnaMaged <yomnaMaged>
2023-02-17 16:57:07 -0800

      Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Hexadecimal base !
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+	}
+	for (x ='a' ; x <= 'f' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
